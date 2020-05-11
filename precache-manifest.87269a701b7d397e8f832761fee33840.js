317527a8a0972467c2346281e201e82614247b71
jjuncoder <jjune21@gmail.com>
2020-05-11 18:13:16 +0900

    Updates

---
+++ precache-manifest.87269a701b7d397e8f832761fee33840.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8388f3336399cb7e26990caa9dc0736",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "f5959358a11e6d0a7660",
+    "url": "/movie-app/static/js/2.df5064f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.df5064f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a14ccdc3452aac075ec",
+    "url": "/movie-app/static/js/main.0531a858.chunk.js"
+  },
+  {
+    "revision": "eb2afba229eb5426c33e",
+    "url": "/movie-app/static/js/runtime-main.6de98e8b.js"
+  }
+]);
